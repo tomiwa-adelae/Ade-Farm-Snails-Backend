fefcafceeff282b362d646f0bf695312f1f8fabd
thetommedia@gmail.com <thetommedia@gmail.com>
2022-08-03 15:35:14 +0100

    Final Commit

--- configs/keys.js
+++ configs/keys.js
@@ -1,2 +1,2 @@
-export const MAIL_JET_API_KEY = '7ffc58acc52f1f82f9a6539e4ad492cb';
-export const MAIL_JET_SECRET_KEY = '1ee14ed6275fbbc106c6779ae905d200';
+export const MAIL_JET_API_KEY = '37e885c3ff6ec0b8657b7210432cef55';
+export const MAIL_JET_SECRET_KEY = '20a5d14335269f6e8ea572e8ed8b2b6a';
